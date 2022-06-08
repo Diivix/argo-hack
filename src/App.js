40a18059f2083c0baf3df42575ac5246196bc364
Chris <chrisiivix@gmail.com>
2022-06-08 12:41:13 +1000

    Added new feature :)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import logo from './logo.svg';
+import iconWhite from './argo-icon-white.png';
+import iconBlack from './argo-icon-black.png';
+import iconBlue from './argo-icon-blue.png';
+import iconGreen from './argo-icon-green.png';
+
 import './App.css';
 
 function App() {
+  let envName = 'blue';
+  let envColor = '#17306E';
+  let envIcon = iconBlue;
+  if (process.env.REACT_APP_ARGO_ENV === 'green') {
+    envName = 'green';
+    envColor = '#114A1F';
+    envIcon = iconGreen;
+  }
+
+  const sendAlert = () => {
+    alert("Nothing interesting here...")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Luna</h1>
+        <div className='App-info'>
+          <p>Continuos Deployments with ArgoCD</p>
+          <p>
+            You are viewing our NEW FEATURE on the <span color={envColor}>{envName.toLocaleUpperCase()} Environment</span>
+          </p>
+          <button className='App-button' onClick={sendAlert}>Find Interesting Stuff</button>
+        </div>
+        <img src={envIcon} className="jump" alt="logo" width={200} height={200} />
       </header>
     </div>
   );
